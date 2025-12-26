e12c7ed5c27a1916a5bca5d5ed83aead689a42f2
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-26 17:49:14 +0530

    Get to normal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
-import Greeting from './components/Greeting'
-import UserCard from './components/UserCard'
 
 function App() {
- console.log(UserCard)
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>TypeScript+React Exercises</h1>
-      <Greeting name='Nikhil' />
-      <UserCard name='Nikhil' age={18} isOnline={true} city='Delhi' />
+     
     </>
   )
 }
