d2698ea1048fdcfa24e212a24d9117be7b56e06c
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-27 17:05:57 +0530

    clean the app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-import Counter from './components/Counter'
-import ObjectState from './components/ObjectState'
-import StringState from './components/StringState'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>TypeScript+React Exercises</h1>
-      <Counter /><br />
-      <StringState /><br />
-      <ObjectState/>
+      
     </>
   )
 }
