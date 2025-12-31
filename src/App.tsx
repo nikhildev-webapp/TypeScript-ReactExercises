6835c4eab8de22409b338636f86b83b3c7eb42d2
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-31 11:57:23 +0530

    Define structure of form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>TypeScript+React Exercises</h1>
-      
+      <Form/>
     </>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,32 @@
-// import React from 'react'
+import { useState } from "react"
 
+// import React from 'react'
 const Form = () => {
+    const containerStyle:any = {
+        border: '1px solid white',
+        display: 'inline-block',
+        padding: '10px',
+        margin: ' 10px',
+        position: 'absolute',
+        top: '30%',
+        left:'50%',
+        transform:'translate(-50%,-50%)'
+    }
+    const [name, setName] = useState<string>('');
+    const [phone, setPhone] = useState<number>();
   return (
-    <div>Form</div>
+      <>
+          <div  style={containerStyle}>
+              <form>
+                  <h2>TestForm</h2>
+                  <label>Name: </label>
+                  <input  type="text" placeholder="Enter your Name.."  value={name}/><br />
+                  <label >Phone:</label>
+                  <input  type="tel" placeholder="Enter Phone Number.." value={phone} /><br />
+                  <button style={{padding:'5px',borderRadius:'10px',position:'relative',left:'30%', marginTop:'10px'}}>SaveData</button>
+              </form>
+          </div>
+      </>
   )
 }
 
