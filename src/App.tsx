cfd583521751d0e95b4d2c6f1933645bc08ee564
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-01 11:54:05 +0530

    clean app.tsx file for next exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
-import CheckBoxState from './components/CheckBoxState'
-import List from './components/List'
-
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>TypeScript+React Exercises</h1>
-      <CheckBoxState />
-      <List/>
+     
     </>
   )
 }
